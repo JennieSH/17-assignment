794fcef18aa2efb3c2763038271149fd1d05cb28
jennie.shao <m65pjj@gmail.com>
2022-01-14 18:19:11 +0800

    style: add normalize.css

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,10 +6,12 @@ export default defineNuxtConfig({
     strict: true
   },
   buildModules: [
+    // https://go.nuxtjs.dev/typescript
     "@nuxt/typescript-build",
     "@nuxtjs/style-resources"
   ],
   alias: { "@": __dirname },
+  css: ["normalize.css"],
   vite: {
     css: {
         preprocessorOptions: {
