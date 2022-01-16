4189982a96efa2eb90d8da86d64207a4da446c81
jennie.shao <m65pjj@gmail.com>
2022-01-16 18:00:40 +0800

    feat: add Button

---
+++ components/Button.vue
@@ -0,0 +1,19 @@
+<template>
+  <button class="btn"><slot></slot></button>
+</template>
+
+<style lang="scss" scoped>
+.btn {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 7px;
+  color: var(--secondary);
+  background-color: var(--primary);
+  border-radius: $rounded;
+
+  &:hover {
+    background-color: var(--active);
+  }
+}
+</style>
