bd2cfe7026291fa08b03dcc8f3971dfeff2698a6
jennie.shao <m65pjj@gmail.com>
2022-01-16 23:53:39 +0800

    feat: add List

---
+++ components/List.vue
@@ -0,0 +1,34 @@
+<template>
+  <ul class="list">
+    <Item
+      v-for="task in list"
+      :key="task.id"
+      :task="task"
+      @toggle-task="$emit('toggle-task', task.id)"
+      @delete="$emit('delete', task.id)"
+    />
+  </ul>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "@nuxtjs/composition-api";
+
+export default defineComponent({
+  props: {
+    list: {
+      type: Array,
+      default: () => []
+    }
+  }
+});
+</script>
+
+<style lang="scss" scoped>
+.list {
+  padding: 10px 10px 20px 10px;
+
+  @include md-width {
+    padding: 20px 40px;
+  }
+}
+</style>
