f36fd3f7a8cc8194bae3c688a7e8af058d55a22b
jennie.shao <m65pjj@gmail.com>
2022-01-16 18:00:21 +0800

    feat: add Icon

---
+++ components/Icon.vue
@@ -0,0 +1,54 @@
+<template>
+  <svg
+    class="icon"
+    :class="`icon--${size}`"
+    fill="currentColor"
+    aria-hidden="true"
+  >
+    <use :xlink:href="`#${name}`" />
+  </svg>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from "vue";
+
+type IconSize = "sm" | "md" | "lg";
+
+export default defineComponent({
+  name: "Icon",
+  props: {
+    className: {
+      type: String,
+      default: ""
+    },
+    name: {
+      type: String,
+      required: true
+    },
+    size: {
+      type: String as PropType<IconSize>,
+      default: "md"
+    }
+  }
+});
+</script>
+
+<style lang="scss">
+.icon {
+  cursor: pointer;
+  &--sm {
+    height: 21px;
+    width: 21px;
+  }
+
+  &--md {
+    height: 24px;
+    width: 24px;
+  }
+
+  &--lg {
+    height: 32px;
+    width: 32px;
+  }
+}
+</style>
