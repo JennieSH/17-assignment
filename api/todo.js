41e40b39d2f58506af1523d33cff6dcf306d427b
jennie.shao <m65pjj@gmail.com>
2022-01-16 23:38:48 +0800

    feat: add fake api methods

---
+++ api/todo.js
@@ -0,0 +1,30 @@
+import { getLocalItem, setLocalItem } from "@/utils/localStorage";
+
+const LOCAL_STORAGE_KEY = "todo";
+
+// 取得任務 api
+const getFakeApi = () => {
+  if (process.server) return Promise.resolve([]);
+
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      const localData = getLocalItem(LOCAL_STORAGE_KEY);
+
+      resolve(localData ? JSON.parse(localData) : []);
+    }, 500);
+  });
+};
+
+// 更新任務的 api
+const setFakeApi = (data) => {
+  if (process.server) return Promise.resolve("skip");
+
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      setLocalItem(LOCAL_STORAGE_KEY, data);
+      resolve("done !");
+    }, 500);
+  });
+};
+
+export { getFakeApi, setFakeApi };
