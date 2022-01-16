e27e01a8c5639d52e466812b6d5beee044ab093f
jennie.shao <m65pjj@gmail.com>
2022-01-16 18:02:07 +0800

    feat: add Tab

---
+++ components/Tab.vue
@@ -0,0 +1,60 @@
+<template>
+  <div
+    class="tab"
+    :class="{
+      'tab--active': active
+    }"
+  >
+    <slot></slot>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  props: {
+    active: {
+      type: Boolean,
+      default: false
+    }
+  }
+});
+</script>
+
+<style lang="scss" scoped>
+.tab {
+  width: 100%;
+  padding: 4px 8px;
+  border: 1px dashed var(--border);
+  border-top-right-radius: $rounded;
+  border-top-left-radius: $rounded;
+  border-bottom-style: solid;
+  color: lighten($gray, 15%);
+  cursor: pointer;
+  text-align: center;
+  background-color: lighten($gray, 35%);
+
+  @include md-width {
+    padding: 8px 0;
+    border-top-right-radius: 0;
+    border-bottom-left-radius: $rounded;
+    border-bottom-style: dashed;
+    border-right-style: solid;
+  }
+
+  &.tab--active,
+  &:hover {
+    color: $gray;
+    font-weight: $bold;
+    border: 1px solid var(--border);
+    border-bottom-color: var(--secondary);
+    background-color: var(--secondary);
+
+    @include md-width {
+      border-bottom-color: var(--border);
+      border-right-color: var(--secondary);
+    }
+  }
+}
+</style>
