ff0ada3806370e58e70cb0dacf451447898c2fe7
jennie.shao <m65pjj@gmail.com>
2022-01-16 23:52:17 +0800

    feat: add Loading

---
+++ components/Loading.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="loading"></div>
+</template>
+
+<style lang="scss" scoped>
+.loading {
+  border-radius: 50%;
+  width: 40px;
+  height: 40px;
+  border: 4px solid lighten($gray, 20%);
+  border-top-color: var(--primary);
+  animation: spin 1s infinite linear;
+}
+</style>
