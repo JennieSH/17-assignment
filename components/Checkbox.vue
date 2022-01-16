114afedc515641ef9c07b037636b2790e77beb64
jennie.shao <m65pjj@gmail.com>
2022-01-16 18:01:00 +0800

    feat: add Checkbox

---
+++ components/Checkbox.vue
@@ -0,0 +1,32 @@
+<template>
+  <Icon
+    :name="active ? 'checkbox-active' : 'checkbox'"
+    class="checkbox"
+    :class="{
+      'checkbox--active': active
+    }"
+  />
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  props: {
+    active: {
+      type: Boolean,
+      default: false
+    }
+  }
+});
+</script>
+
+<style lang="scss" scoped>
+.checkbox {
+  color: var(--border);
+
+  &--active {
+    color: var(--active);
+  }
+}
+</style>
