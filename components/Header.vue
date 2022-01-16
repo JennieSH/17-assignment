b5f10df6f4bc4262f861b6377f310ce2253f8ea0
jennie.shao <m65pjj@gmail.com>
2022-01-16 18:01:38 +0800

    feat: add Header

---
+++ components/Header.vue
@@ -0,0 +1,34 @@
+<template>
+  <header>
+    <Icon name="burger" size="lg" />
+    <h1>HandsUP</h1>
+    <Icon name="sign-out" size="lg" />
+  </header>
+</template>
+
+<style lang="scss" scoped>
+header {
+  position: fixed;
+  z-index: 10;
+  width: 100%;
+  height: 55px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 12px;
+  color: var(--secondary);
+  background-color: var(--primary);
+
+  h1 {
+    flex-grow: 1;
+    font-size: $text-md;
+    text-align: center;
+
+    @include lg-width {
+      text-align: start;
+      font-size: $text-lg;
+      padding-left: 15px;
+    }
+  }
+}
+</style>
